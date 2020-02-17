c1909892de382e9679289b4c121115209dc750ce
James Ousby <joousby@amazon.com>
2020-02-18 05:42:04 +1100

    --wip-- [skip ci]

--- cdk-infra/src/main/java/software/amazon/samples/EventSourcingInfraApp.java
+++ cdk-infra/src/main/java/software/amazon/samples/EventSourcingInfraApp.java
@@ -6,14 +6,32 @@ import software.amazon.awscdk.core.StackProps;
 
 public class EventSourcingInfraApp {
 
+    private static Environment getEnv() {
+        // are we running in CodeBuild?
+        // If so passing in the account and region to cdk helps contextualise VPC generation (this is clunky in
+        // my opinion).
+        String codeBuildARN = System.getenv("CODEBUILD_BUILD_ARN");
+        if (codeBuildARN != null) {
+            // expected format: arn:aws:codebuild:region-ID:account-ID:build/codebuild-demo-project:b1e6661e-e4f2-4156-9ab9-82a19EXAMPLE
+            String[] tokens = codeBuildARN.split(":");
+            String regionId = tokens[3];
+            String accountId = tokens[4];
+
+            return Environment.builder()
+                .account(accountId)
+                .region(regionId)
+                .build();
+        }
+
+        return Environment.builder().build();
+    }
+
     public static void main(final String[] args) {
         App app = new App();
 
         new EventSourcingInfraStack(app, "EventSourcingInfraStack",
             StackProps.builder()
-                .env(Environment.builder()
-                    .region("ap-southeast-2") // triggers vpc to create a 3 az instead of 2 az setup
-                    .build())
+                .env(getEnv())
                 .build());
 
         app.synth();
