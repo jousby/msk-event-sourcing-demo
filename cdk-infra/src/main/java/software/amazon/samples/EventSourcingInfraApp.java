d4f0eb74b57e6652de212a0bae2c6a61408cf86f
James Ousby <joousby@amazon.com>
2020-02-17 16:44:26 +1100

    --wip-- [skip ci]

--- cdk-infra/src/main/java/software/amazon/samples/EventSourcingInfraApp.java
+++ cdk-infra/src/main/java/software/amazon/samples/EventSourcingInfraApp.java
@@ -1,12 +1,20 @@
 package software.amazon.samples;
 
 import software.amazon.awscdk.core.App;
+import software.amazon.awscdk.core.Environment;
+import software.amazon.awscdk.core.StackProps;
 
 public class EventSourcingInfraApp {
+
     public static void main(final String[] args) {
         App app = new App();
 
-        new EventSourcingInfraStack(app, "EventSourcingInfraStack");
+        new EventSourcingInfraStack(app, "EventSourcingInfraStack",
+            StackProps.builder()
+                .env(Environment.builder()
+                    .region("ap-southeast-2") // triggers vpc to create a 3 az instead of 2 az setup
+                    .build())
+                .build());
 
         app.synth();
     }
--- cdk-pipeline/src/main/java/software/amazon/samples/EventSourcingPipelineApp.java
+++ cdk-pipeline/src/main/java/software/amazon/samples/EventSourcingPipelineApp.java
@@ -1,12 +1,19 @@
 package software.amazon.samples;
 
 import software.amazon.awscdk.core.App;
+import software.amazon.awscdk.core.Environment;
+import software.amazon.awscdk.core.StackProps;
 
 public class EventSourcingPipelineApp {
     public static void main(final String[] args) {
         App app = new App();
 
-        new EventSourcingPipelineStack(app, "EventSourcingPipelineStack");
+        new EventSourcingPipelineStack(app, "EventSourcingPipelineStack",
+            StackProps.builder()
+                .env(Environment.builder()
+                    .region("ap-southeast-2")
+                    .build())
+                .build());
 
         app.synth();
     }
